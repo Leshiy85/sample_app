f900aaba23a9662cc0b3764db9c953be4be334b8
Leshiy85 <Leshiy85.85@mail.ru>
2015-06-04 13:01:04 +0300

    Make a basic user model (including secure passwords)

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,13 +1,33 @@
 require 'spec_helper'
 
-describe "User pages" do
+describe User do
 
-  subject { page }
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+  describe "when name is too long" do
+    before { @user.name = "a" * 51 }
+    it { should_not be_valid }
+  end
 
-  describe "signup page" do
-    before { visit signup_path }
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                     foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        expect(@user).not_to be_valid
+      end
+    end
+  end
 
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid
+      end
+    end
   end
 end
